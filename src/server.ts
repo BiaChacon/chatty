2236cf177deb4c157696fa7bf53727986b52880d
Bia Chacon <anabheatrizchacon@outlook.com>
2021-04-19 16:12:40 -0300

    start

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({
+    message : "Hello World!"
+  });
+});
+
+app.post("/", (request, response) => {
+  return response.json({message: "Usuário salvo com sucesso!"})
+});
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
